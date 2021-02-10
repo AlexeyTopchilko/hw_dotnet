f42cf1b8db421ae70535e4159798774951f087ae
User <User@USER->
2021-02-10 22:52:20 +0300

    Added new project for homework

---
+++ HW.03.Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW._03.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
