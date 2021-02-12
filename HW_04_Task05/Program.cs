c4f9392b02fc916be90797003902d524f1ca0c27
User <User@USER->
2021-02-12 23:18:34 +0300

    Added HW_04_Task05

---
+++ HW_04_Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW_04_Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int unicodeDec = 90; unicodeDec > 64; unicodeDec--)
+            {
+                char letter = Convert.ToChar(unicodeDec);
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
