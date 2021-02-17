4eeba9f611dcb41a016b84b45c00ff4f9f447f5d
User <User@USER->
2021-02-17 17:36:48 +0300

    Added HW_05_Task3

---
+++ HW_05/HW_05_Task3/Greeting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_05_Task3
+{
+    class Greeting
+    {
+        public static void GoodDay()
+        {
+            int currentHour = DateTime.Now.TimeOfDay.Hours;
+            switch (currentHour)
+            {
+                case 9 :
+                case 10:
+                case 11:
+                case 12:
+                Console.WriteLine("Good Morning!");
+                    break;
+
+                case 13:
+                case 14:
+                case 15:
+                Console.WriteLine("Good Day!");
+                    break;
+
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                        Console.WriteLine("Good evening!");
+                    break;
+
+                default:
+                        Console.WriteLine("Somthing going wrong!");
+                    break;
+        }
+    }
+}
+}
---
+++ HW_05/HW_05_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_05_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeting.GoodDay();
+        }
+    }
+}
