6cb91bdc34bf70439a7b0b6de6aaebd7e9e33808
User <User@USER->
2021-02-17 17:49:05 +0300

    Added HW_05_Task4

---
+++ HW_05/HW_05_Task4/LetterCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_05_Task4
+{
+    class LetterCounter
+    {
+        public static void ACounter()
+        {
+            int count = 0;
+            Console.WriteLine("Please input a word");
+            string word = Console.ReadLine();
+            for (int i = 0; i < word.Length ; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'A' || word[i] == 'А' || word[i] == 'а')
+                    ++count;
+            }
+            Console.WriteLine("Number of 'A' is " + count);
+        }
+    }
+}
