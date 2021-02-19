28bf0e3f5e1f48e853908daec5ea2b6b160307a2
User <User@USER->
2021-02-19 20:05:12 +0300

    Added HW.06.Task3

---
+++ HW_06/HW.06.Task3/Massiv.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._06.Task3
+{
+    class Massiv
+    {
+        public static int GetSize()
+        {
+            Console.WriteLine("Please input size of array");
+            bool chek = Int32.TryParse(Console.ReadLine(), out int size);
+            while (chek == false || size <= 0)
+            {
+                Console.WriteLine("Please input correct size of array");
+                chek = Int32.TryParse(Console.ReadLine(), out size);
+            }
+            return size;
+        }
+
+        public static int[] RandomMass(int size)
+        {
+            int[] randomMass = new int[size];
+            Random num = new Random();
+            for (int i = 0; i < size; i++)
+                randomMass[i] = num.Next();
+
+            return randomMass;
+        }
+
+        public static int[] Reverse(int[] mass)
+        {
+            int size = mass.Length;
+            for(int i =0;i <= (size-1)/2;i++)
+            {
+                int buffer = mass[size- 1 - i];
+                mass[size - 1 - i] = mass[i];
+                mass[i] = buffer;
+            }
+            return mass;
+        }
+    }
+}
---
+++ HW_06/HW.06.Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW._06.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = Massiv.RandomMass(Massiv.GetSize());
+            foreach (int item in mass)
+                Console.Write(item+" ");
+            Console.WriteLine();
+            Massiv.Reverse(mass);
+            foreach (int item in mass )
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
