cff77ca6b6fdf7f94db37b0df3d7033b24b51a85
User <User@USER->
2021-02-26 14:58:10 +0300

    Added HW.08.Task1

---
+++ HW.08.Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace HW._08.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string @string = GetString();
+            Console.WriteLine(@string);
+            string newString = ChangeString(@string);
+            Console.WriteLine(newString);
+        }
+        static string GetString()
+        {
+            Console.WriteLine("Please input a string: ");
+            string @string = Console.ReadLine();
+            bool chek = @string.Contains('?');
+            while (!chek)
+            {
+                Console.WriteLine("Please input another string");
+                @string = Console.ReadLine();
+                chek = @string.Contains('?');
+            }
+            return @string;
+        }
+
+        static string ChangeString(string @string)
+        {
+            StringBuilder sb = new StringBuilder(@string);
+            int i;
+            for (i = 0; i < sb.Length; i++)
+            {
+                if (sb[i].Equals('?'))
+                    break;
+                else if (sb[i].Equals('!') || sb[i].Equals('.'))
+                {
+                    sb.Remove(i, 1);
+                    --i;
+                }    
+            }
+            sb.Replace(' ', '_', i, sb.Length - i);
+            string newString = sb.ToString();
+            return newString;
+        }
+    }
+}
