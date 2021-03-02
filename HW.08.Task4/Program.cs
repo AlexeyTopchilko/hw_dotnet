3cee7bf18949576fabee5d5ebfda62cc266c05f4
User <User@USER->
2021-03-02 20:29:21 +0300

    Added HW.08.Task4

---
+++ HW.08.Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HW._08.Task4
+{
+    class Program
+    {
+        const double Pi = 3.14;
+        double var = 10.0;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
